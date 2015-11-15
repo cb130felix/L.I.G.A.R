b82b36cb062b7fb72ae7fd932190c81c19e6d818
iagorichard <iagorichard@iagorichard-X450LD>
2015-11-15 15:40:11 -0300

    Implementaçao do metodo 'connectionServer'

--- Documents/NetBeansProjects/Middleware/src/middleware/ManagerConnection.java
+++ Documents/NetBeansProjects/Middleware/src/middleware/ManagerConnection.java
@@ -14,29 +14,50 @@ import java.net.Socket;
  * @author Iago
  */
 public class ManagerConnection {
- 
+
     private ServerSocket serverSocket;
     private Socket connection;
     private DatagramSocket broadcast;
-    
+
     //manda mensagem em broadcast
-    public boolean broadcast(){return true;}
-    
+    public boolean broadcast() {
+        return true;
+    }
+
     //Se conecta a uma máquina de acordo com o endereço passado
-    public boolean connectionServer(Adress serverAdress){return true;}
-    
+    public boolean connectionServer(Adress serverAdress) {
+
+        try {
+            this.connection = new Socket(serverAdress.getIp(), serverAdress.getPort());
+            return true;
+        } catch (Exception e) {
+            System.out.println("Não foi possível estabelecer uma conexão com o servidor.");
+            System.out.println("Erro:");
+            e.printStackTrace();
+            return false;
+        }
+    }
+
     //Esse método fica escutando mensagens na porta informada e atualiza
     //o socket dessa classe de acordo com o endereço da máquina que fez a requisição
-    public boolean listener(int port){return true;}
-    
+    public boolean listener(int port) {
+        return true;
+    }
+
     //método para envio e recebimento de dados
-    public boolean send(byte[] data){return true;}
-    public byte[] receive(){return null;}
-    
+    public boolean send(byte[] data) {
+        return true;
+    }
+
+    public byte[] receive() {
+        return null;
+    }
+
     //checa se há conexão com a internet
-    public boolean checkConnection(){return true;}
-    
-    
+    public boolean checkConnection() {
+        return true;
+    }
+
 }
 
 // Fluxo de uma aplicação com essa classe:
